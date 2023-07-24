405cf179cacd58fd1d04b69d4d0ac89f6390e548
AJ Slater <aj@slater.net>
2023-07-23 17:20:26 -0700

    fix roman for darwin

--- bin/roman.sh
+++ bin/roman.sh
@@ -18,10 +18,15 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
+if command -v ggrep &>/dev/null; then
+    GREP_CMD=$(command -v ggrep)
+else
+    GREP_CMD=$(command -v grep)
+fi
 # get files
 fns=$(find "$@" -type f -name "*.sh")
 if [ "$ignorefile" ]; then
-    fns=$(echo "$fns" | grep -vf "$ignorefile")
+    fns=$(echo "$fns" | "$GREP_CMD" -vf "$ignorefile")
 fi
 
 # find nonconforming files
@@ -29,13 +34,13 @@ good=1
 while read -ra fn; do
     # sc doesn't understand that fn isn't an array
     # shellcheck disable=2128
-    bad=$(sed -n '2 p' <"$fn" | grep -v '^#\ +*')
+    bad=$(sed -n '2 p' <"$fn" | "$GREP_CMD" -v '^# +*')
     if [ "$bad" ]; then
         # shellcheck disable=2128
         echo "🔪 $fn"
         good=0
     fi
-  done < <(echo "$fns")
+done < <(echo "$fns")
 
 if [ "$good" = 0 ]; then
     exit 1
