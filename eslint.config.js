8e94df6ba2d02876e44e842d74af664758c08abd
AJ Slater <aj@slater.net>
2024-09-05 20:33:59 -0700

    add no-unsantized and no-ue-extend-native back

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,8 @@ import eslintPluginDepend from "eslint-plugin-depend";
 import eslintPluginJsonc from "eslint-plugin-jsonc";
 import eslintPluginMarkdown from "eslint-plugin-markdown";
 import eslintPluginNoSecrets from "eslint-plugin-no-secrets";
-// import eslintPluginNoUnsanitized from "eslint-plugin-no-unsanitized";
-// https://github.com/mozilla/eslint-plugin-no-unsanitized/issues/241
-// import eslintPluginNoUseExtendNative from "eslint-plugin-no-use-extend-native";
-// Warnings break circleci build
+import eslintPluginNoUnsanitized from "eslint-plugin-no-unsanitized";
+import eslintPluginNoUseExtendNative from "eslint-plugin-no-use-extend-native";
 import eslintPluginPrettier from "eslint-plugin-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import eslintPluginPromise from "eslint-plugin-promise";
@@ -51,8 +49,8 @@ export default [
   eslintPluginDepend.configs[FLAT_RECOMMENDED],
   ...eslintPluginJsonc.configs["flat/recommended-with-jsonc"],
   ...eslintPluginMarkdown.configs.recommended,
-  // eslintPluginNoUseExtendNative.configs.recommended,
-  // eslintPluginNoUnsanitized.configs.recommended,
+  eslintPluginNoUseExtendNative.configs.recommended,
+  ...eslintPluginNoUnsanitized.configs.recommended,
   eslintPluginPrettierRecommended,
   eslintPluginPromise.configs[FLAT_RECOMMENDED],
   eslintPluginRegexp.configs[FLAT_RECOMMENDED],
@@ -77,8 +75,6 @@ export default [
       jsonc: eslintPluginJsonc,
       markdown: eslintPluginMarkdown,
       "no-secrets": eslintPluginNoSecrets,
-      // "no-use-extend-native": eslintPluginNoUseExtendNative,
-      // "no-unsantized": eslintPluginNoUnsanitized,
       prettier: eslintPluginPrettier,
       promise: eslintPluginPromise,
       security: eslintPluginSecurity,
