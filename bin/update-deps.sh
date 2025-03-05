11b171cec63c7150010ec3b20b6b4c3f87c5ae3c
AJ Slater <aj@slater.net>
2025-03-05 11:14:42 -0800

    fix update deps to include extras

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update python and npm dependencies
 set -euo pipefail
-uv sync --no-install-project
+uv sync --no-install-project --all-extras
 uv tree --outdated
 npm update
 npm outdated
